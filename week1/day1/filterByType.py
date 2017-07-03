f2e2e07f327f76330d05e0e63bcec4d518c20414
Justin Dietz <justin.r.dietz@gmail.com>
2017-07-03 11:48:57 -0700

    filterByType

---
+++ week1/day1/filterByType.py
@@ -0,0 +1,50 @@
+def isItBig(num):
+    if num >= 100:
+        return 'That\'s a big number!'
+    return 'That\'s a small number'
+
+
+def isItLong(str):
+    if len(str) >= 50:
+        return 'long sentence'
+    return 'short sentence'
+
+
+def isItLongList(lst):
+    if len(lst) >= 10:
+        return 'Big List'
+    return 'Short List'
+
+
+sI = 45
+mI = 100
+bI = 455
+eI = 0
+spI = -23
+sS = "Rubber baby buggy bumpers"
+mS = "Experience is simply the name we give our mistakes"
+bS = "Tell me and I forget. Teach me and I remember. Involve me and I learn."
+eS = ""
+aL = [1, 7, 4, 21]
+mL = [3, 5, 7, 34, 3, 2, 113, 65, 8, 89]
+lL = [4, 34, 22, 68, 9, 13, 3, 5, 7, 9, 2, 12, 45, 923]
+eL = []
+spL = ['name', 'address', 'phone number', 'social security number']
+
+
+print isItBig(sI)
+print isItBig(mI)
+print isItBig(bI)
+print isItBig(eI)
+print isItBig(spI)
+
+print isItLong(sS)
+print isItLong(mS)
+print isItLong(bS)
+print isItLong(eS)
+
+print isItLongList(aL)
+print isItLongList(mL)
+print isItLongList(lL)
+print isItLongList(eL)
+print isItLongList(spL)
