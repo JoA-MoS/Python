0182ce8419ad6d457002711f33802195e5a2ef29
Justin Dietz <justin.r.dietz@gmail.com>
2017-07-06 12:49:33 -0700

    nija and hacker leverl

--- week1/day3/callcenter.py
+++ week1/day3/callcenter.py
@@ -39,18 +39,40 @@ class CallCenter(object):
             print c
         print 'Queue Length: ' + str(self.queue)
 
+    def getCallByNumber(self, number):
+        for c in self.calls:
+            if c.number == number:
+                return c
+
+    def removeCallByNumber(self, number):
+        for c in self.calls:
+            if c.number == number:
+                self.calls.remove(c)
+        return self
+
+    def sortByCallTime(self):
+        self.calls.sort(key=lambda x: x.time)
+        return self
+
 
 c1 = Call('Justin', '5092304274', 'Just Testing')
 
-cc = CallCenter([Call('Justin', '5092304274', 'Just Testing'),
+cc = CallCenter([Call('Justin', '5092304274', 'Just Testing', datetime.datetime(2017, 1, 12)),
                  Call('Justin', '1234567890', 'Just Testing'),
                  Call('Justin', '098', 'Just Testing'),
                  Call('Justin', '5092304274', 'Just Testing'),
-                 Call('Justin', '5092304274', 'Just Testing'), ])
+                 Call('Justin', '5092304274', 'Just Testing', datetime.datetime(2016, 1, 12)), ])
 
 
 cc.info()
 cc.remove()
 cc.info()
 cc.add(c1)
+cc.info()
+print '========================'
+print cc.getCallByNumber('098')
+cc.removeCallByNumber('098')
+cc.info()
+print '========================'
+cc.sortByCallTime()
 cc.info()
