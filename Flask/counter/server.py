fdc507e1f2252e993cd17b65d6c98be8f412cddc
Justin Dietz <justin.r.dietz@gmail.com>
2017-07-10 12:56:13 -0700

    Counter, couldn't get post to work

--- Flask/counter/server.py
+++ Flask/counter/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, session, render_template
+from flask import Flask, session, render_template, redirect
 
 app = Flask(__name__)
 app.secret_key = 'ThisIsSecret'
@@ -10,6 +10,23 @@ def counter():
         session['count'] += 1
     else:
         session['count'] = 0
+    return render_template('index.html', count=session['count'])
+
+
+@app.route('/count/add/<val>')
+def add(val):
+    val = int(val)
+    if 'count' in session:
+        session['count'] += val
+    else:
+        session['count'] = val
+    return redirect('/')
+
+
+@app.route('/count/reset')
+def reset():
+
+    session['count'] = 0
     return render_template('index.html', count=session['count'])
 
 
---
+++ Flask/counter/static/js/client.js
@@ -0,0 +1,18 @@
+// window.onload = function () {
+//     console.log('im here');
+//     // console.log(document.getElementsByClassName('post-anchor'))
+//     console.log($('.post-anchor'));
+
+//     $('.post-anchor').click(function (e) {
+//         e.preventDefault();
+//         console.log('you clicked me');
+//         postToUrl($(this).attr('href'));
+//         return false;
+//     });
+// }
+
+// function postToUrl(url) {
+//     console.log('post to url');
+//     $.post(url);
+//     window.location = '/';
+// }
